33f3137a59e0b048615f0f449a55262f5c63998e
dohyunkim <kdh951102@gmail.com>
2022-02-14 18:06:19 +0900

    Done on lecture13

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,11 @@
+const country = "Seoul";
+const continent = "Asia";
+let population = 300000;
+
+const isIsland = false;
+const language = "Korean";
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,76 @@
+// let js = 'amazing';
+
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('Jonas');
+// console.log(23);
+
+// let firstName = 'Matilda'; //declaring varliable
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// //let 3years = 'blah'  // Occur Err. (Syntax err)
+
+// // Cannot naming variable like this
+// // let 3years = 3;
+// // let new = 27;
+// // let function = 27;
+// // let Person = 'jonas';  -> let person (lower case)
+
+// let PI = 3.1415;
+
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Teacher';
+
+// let job1 = 'programmer';
+// let job2 = 'teacher';
+
+// console.log(myFirstJob);
+
+// //ASSIGNMENT
+// let country = 'Seoul';
+// let continent = 'YeokSam';
+// let population = 30000;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+/////////////////////////
+//Data Types
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true); //typeof 는 + 나 - 와 같은 연산자일 뿐이다. type을 반환함.
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 'Dohyun');
+// console.log(typeof 23);
+
+// javascriptIsFun = 'fuck'; //재할당. (without let선언)
+// console.log(javascriptIsFun);
+
+// let year; //Undefined
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null); //error. typeof null 은 null을 반환해야 함. object 아님.
+
+////////////////////////
+//let , const and var
+let age = 30;
+age = 31; // mutate variable
+
+const birthYear = 1995;
+//birthYear = 1990; // occur err! immutable variable
+// const job; //illegal (without initial value)
+
+// 나중에 변하지 않을 값은 무조건 const 를 사용할것. (default로 const 를 사용한다는 mind)
+// var는 절대 쓰지 말 것.
+
+lastName = "Kim"; // terrible!
+console.log(lastName);
