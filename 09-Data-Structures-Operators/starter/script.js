2c5d6f6e82dd5f2dbafe2a1d3b906a8c4488392b
dohyunkim <kdh951102@gmail.com>
2022-03-01 16:36:44 +0900

    Destructuring Array & Obj

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,25 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1, // default values
+    mainIndex = 1,
+    time = '19:00',
+    address = 'funky town',
+  }) {
+    console.log(
+      'Order received!',
+      this.starterMenu[starterIndex],
+      this.mainMenu[mainIndex],
+      time,
+      address
+    );
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +46,82 @@ const restaurant = {
     },
   },
 };
+
+////////////////////////////////////
+// Destructuring Objects
+// const deliveryObj = {
+//   time: '22:30',
+//   address: 'Via del Sole, 21',
+//   mainIndex: 2,
+//   starterIndex: 2,
+// };
+
+// restaurant.orderDelivery(deliveryObj); // Object 하나만 보낸다. 그러면 fucntion에서 자체적으로 destructuring.
+// restaurant.orderDelivery({
+//   mainIndex: 0,
+// });
+
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+// // With new variable names
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+
+// console.log(restaurantName, hours, tags);
+
+// // setting default value
+// const { fucky: menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+
+// // Mutating variables
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+// console.log(obj);
+
+// // { blah, blah } = obj  이런식으로 쓰면, js 엔진은 {}를 코드블럭으로 인식, 코드블럭에 = 연산자로 무엇을 대입하는것은 오류로 인식.
+// ({ a, b } = obj); // Good. (소괄호로 감싼다.)
+// console.log(a, b); //It works!
+
+// // Nested Objects
+// const {
+//   fri: { open: o, close: c },
+// } = openingHours;
+// console.log(o, c);
+
+////////////////////////////////////
+// Destructuring Arrays
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// const [x, y, z] = arr; // Destructuring Array. []는 array가 아니라 destructuring하는 것일 뿐.
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// // Switching variables
+// // const temp = main;
+// // main = secondary;
+// // secondary = temp;
+
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// //Receive 2 return values from a function
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// // Nested destructuring
+// const nested = [2, 4, [5, 6]];
+// const [i, j, [k, l]] = nested;
+// console.log(i, j, k, l);
+
+// // Default values
+// const [p = 1, q = 1, r = 1] = [8, 9];
+// console.log(p, q, r);
