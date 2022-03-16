58954e2e85dd5e68441cd1b5aa76b17dcbe5d308
dohyunkim <kdh951102@gmail.com>
2022-03-16 23:54:45 +0900

    Review Array

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -89,7 +89,7 @@ currencies.forEach(function (currency, key, maps) {
   console.log(maps);
 });
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 // // Arrays are object, and can use built-in methods.
