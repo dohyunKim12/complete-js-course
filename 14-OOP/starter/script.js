db34587f8832dbd152e48fcf52d7c826382b0d3e
dohyunkim <kdh951102@gmail.com>
2022-04-05 19:02:47 +0900

    4 Fundamental OOP

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const user = {
+  user: 'dohyun',
+  password: 'ehgus0303!',
+  login(password) {
+    //login logic
+  },
+  sendMessage(str) {
+    //sending logic
+  },
+};
+
+// 4 Fundamental principles of OOP
+// * Abstraction * (추상화)
+// ignore or to hide detail.
+
+// * Encapsulation * (캡슐화)
+// keep some properties and methods Private inside the class. (Not accessible outside of class)
+// but some methods can be exposed as a public interface (API)
+
+// * Inheritance *
+// * Polymorphism *
