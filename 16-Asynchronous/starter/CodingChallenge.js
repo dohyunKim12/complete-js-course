d7ab8150f9e9b34b10ae75b5c49a7c1bf24d792c
dohyunkim <kdh951102@gmail.com>
2022-05-02 18:19:10 +0900

    Review & CC2

--- 16-Asynchronous/starter/CodingChallenge.js
+++ 16-Asynchronous/starter/CodingChallenge.js
@@ -1,23 +1,78 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+// CC 1
+// const btn = document.querySelector('.btn-country');
+// const countriesContainer = document.querySelector('.countries');
 
-btn.addEventListener('click', function () {
-  console.log('fuck');
-});
+// btn.addEventListener('click', function () {
+//   console.log('fuck');
+// });
 
-const whereAmI = function (lat, lng) {
-  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-    .then(response => {
-      if (!response.ok)
-        throw new Error(`Problem with geocoding ${response.status}`);
-      return response.json();
-    })
-    .then(data => {
-      console.log(data.country);
-    })
-    .catch(err => console.error(`${err.message}`));
+// const whereAmI = function (lat, lng) {
+//   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//     .then(response => {
+//       if (!response.ok)
+//         throw new Error(`Problem with geocoding ${response.status}`);
+//       return response.json();
+//     })
+//     .then(data => {
+//       console.log(data.country);
+//     })
+//     .catch(err => console.error(`${err.message}`));
+// };
+
+// whereAmI('37.4994', '127.0334');
+
+///////////////////////////////////////////
+// CC 2
+// Part 1
+const imgContainer = document.querySelector('.images');
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
 };
 
-whereAmI('37.4994', '127.0334');
+// when image is done loading, append it to DOM EL with the 'images' class, and resolve the promise.
+// fullfilled value should be the image element itself.
+
+///////////////////////////////////////////
+// Part 2
+// Consume the promise using .then and also add an error handler.
+//  ...
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+let currentImage;
+
+createImage('./img/img-1.jpg')
+  .then(img => {
+    currentImage = img;
+    return wait(2);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+    return createImage('./img/img-2.jpg');
+  })
+  .then(img => {
+    currentImage = img;
+    return wait(2);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+  })
+  .catch(err => console.error(err));
+// console.log(createImage());
