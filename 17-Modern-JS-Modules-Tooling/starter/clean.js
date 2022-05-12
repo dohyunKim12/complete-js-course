6bb5e16b3629bd45cd6f2bf37588c7b333f5e17f
dohyunkim <kdh951102@gmail.com>
2022-05-12 22:31:27 +0900

    Way of Refactoring

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,64 +1,100 @@
-var budget = [
+'use strict';
+
+const budget = Object.freeze([
+  // Not deep freeze, only shallow freeze
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
-  { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
+  { value: 300, description: 'Freentryancing 👩‍💻', user: 'jonas' },
   { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-var limits = {
+const spendingLimits = Object.freeze({
+  // make immutable
   jonas: 1500,
   matilda: 100,
+  dohyun: 2000,
+});
+
+// const limit = spendingLimits[user] ? spendingLimits[user] : 0;
+// Use optional chaining
+const getLimit = (limits, user) => limits?.[user] ?? 0;
+
+// Pure function :D
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'dohyun'
+) {
+  // if (!user) user = 'jonas'; // change to default parameter
+  const cleanUser = user.toLowerCase();
+
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value: -value, description, user: cleanUser }]
+    : state;
 };
 
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
-  user = user.toLowerCase();
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+console.log(newBudget1);
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  10,
+  'Going to movies 🍿',
+  'Matilda'
+);
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
 
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
-  } else {
-    lim = 0;
-  }
+const checkExpenses = (state, limits) =>
+  state.map(entry =>
+    entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: 'limit' }
+      : entry
+  );
 
-  if (value <= lim) {
-    budget.push({ value: -value, description: description, user: user });
-  }
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+console.log(finalBudget); // chain of addExpense
+
+// Impure function
+const logBigExpenses = function (state, bigLimit) {
+  // let output = '';
+  // for (const entry of budget)
+  //   output +=
+  //     entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
+
+  // output = output.slice(0, -2); // Remove last '/'
+  // console.log(output);
+  const bigExpenses = state
+    .filter(entry => entry.value <= -bigLimit)
+    .map(entry => entry.description.slice(-2))
+    .join(' / ');
+  // .reduce((str, cur) => `${str} / ${cur.description.slice(-2)}`, '');
+
+  console.log(bigExpenses);
 };
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
-console.log(budget);
 
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
-    } else {
-      lim = 0;
-    }
+logBigExpenses(finalBudget, 100);
 
-    if (el.value < -lim) {
-      el.flag = 'limit';
-    }
-  }
-};
-check();
+//////////////////////////////////////
+// Imperative vs Declarative way
 
-console.log(budget);
+// Imperative
+// How to do things. need to explain every single step.
+// Example: step-by-step recipe of a cake
 
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
-    }
-  }
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
+// Declarative
+// Programmer tells "Waht to do"
+// simply describe the way the computer should achieve the result
+// the HOW
+
+// Functional Programming
+// 기본적으로 Declarative programming임.
+// Based on the idea of writing sw by combining many pure functions, avoiding side effects and mutating data
+
+// Side effect: Modification (mutation) of any data outside of the function
+// Pure function: function without side effects.
--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,116 +1,116 @@
-/////////////////////////////////////////////
+// /////////////////////////////////////////////
 
-// Modern Javascript coding 꿀팁.
-// Script 어떻게 분할하고 module을 어떻게 import 하는지,
-// Design pattern 에 대해서 학습.
+// // Modern Javascript coding 꿀팁.
+// // Script 어떻게 분할하고 module을 어떻게 import 하는지,
+// // Design pattern 에 대해서 학습.
 
-// 3-rd party module이 굉장히 많음. (packages. npm repository, jquery, leaflet library,...)
-// coding 만 다 했따고 끝나는 게 아님.
+// // 3-rd party module이 굉장히 많음. (packages. npm repository, jquery, leaflet library,...)
+// // coding 만 다 했따고 끝나는 게 아님.
 
-// Importing module
-// 'use strict' (all modules are executed in strict mode by default)
-// console.log('Top of script.js');
-// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// // Importing module
+// // 'use strict' (all modules are executed in strict mode by default)
+// // console.log('Top of script.js');
+// // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 
-// addToCart('bread', 5);
-// console.log(price, tq);
+// // addToCart('bread', 5);
+// // console.log(price, tq);
 
-// import * as ShoppingCart from './shoppingCart.js';
-// ShoppingCart.addToCart('pizza', 7);
-// console.log(ShoppingCart.totalPrice, ShoppingCart.tq);
+// // import * as ShoppingCart from './shoppingCart.js';
+// // ShoppingCart.addToCart('pizza', 7);
+// // console.log(ShoppingCart.totalPrice, ShoppingCart.tq);
 
-// import add, { cart } from './shoppingCart.js';
-// // 하나의 모듈을 두번 import하는것은 절대 권장x
-// // named export 와 default export를 섞어 쓰는 것도 절대 권장 x
-// // import는 export하는 파일과의 live connection 이라는 것을 보여줌.
-// // not just copied
-// add('hamburger', 9);
-// add('bread', 5);
-// add('apples', 3);
+// // import add, { cart } from './shoppingCart.js';
+// // // 하나의 모듈을 두번 import하는것은 절대 권장x
+// // // named export 와 default export를 섞어 쓰는 것도 절대 권장 x
+// // // import는 export하는 파일과의 live connection 이라는 것을 보여줌.
+// // // not just copied
+// // add('hamburger', 9);
+// // add('bread', 5);
+// // add('apples', 3);
 
-// console.log(cart);
+// // console.log(cart);
 
-////////////////////////////////////////////////
-// TOP - level await (ES2022)
-// console.log('Start fetching');
-// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-// const data = await res.json();
-// console.log(data);
-// // async function 밖에서의 await 키워드는 모듈의 전체 진행을 막는다.
+// ////////////////////////////////////////////////
+// // TOP - level await (ES2022)
+// // console.log('Start fetching');
+// // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// // const data = await res.json();
+// // console.log(data);
+// // // async function 밖에서의 await 키워드는 모듈의 전체 진행을 막는다.
 
-// console.log('Something');
+// // console.log('Something');
 
-// const getLastPost = async function () {
-//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-//   const data = await res.json();
-//   console.log(data);
+// // const getLastPost = async function () {
+// //   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// //   const data = await res.json();
+// //   console.log(data);
 
-//   return { title: data.at(-1).title, text: data.at(-1).body };
-// };
+// //   return { title: data.at(-1).title, text: data.at(-1).body };
+// // };
 
-// const lastPost = getLastPost();
-// console.log(lastPost);
+// // const lastPost = getLastPost();
+// // console.log(lastPost);
 
-// // NOt very clean
-// // lastPost.then(last => console.log(last));
+// // // NOt very clean
+// // // lastPost.then(last => console.log(last));
 
-// const lastPost2 = await getLastPost();
-// console.log();
+// // const lastPost2 = await getLastPost();
+// // console.log();
 
-////////////////////////////////////////////////////////
-// // Module pattern
-// const ShoppingCart2 = (function () {
-//   const cart = [];
-//   const shippingCost = 10;
-//   const totalPrice = 237;
-//   const totalQuantity = 23;
+// ////////////////////////////////////////////////////////
+// // // Module pattern
+// // const ShoppingCart2 = (function () {
+// //   const cart = [];
+// //   const shippingCost = 10;
+// //   const totalPrice = 237;
+// //   const totalQuantity = 23;
 
-//   const addToCart = function (product, quantity) {
-//     cart.push({ product, quantity });
-//     console.log(
-//       `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
-//     );
-//   };
+// //   const addToCart = function (product, quantity) {
+// //     cart.push({ product, quantity });
+// //     console.log(
+// //       `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+// //     );
+// //   };
 
-//   const orderStock = function (product, quantity) {
-//     cart.push({ product, quantity });
-//     console.log(`${quantity} ${product} ordered from supplier`);
-//   };
+// //   const orderStock = function (product, quantity) {
+// //     cart.push({ product, quantity });
+// //     console.log(`${quantity} ${product} ordered from supplier`);
+// //   };
 
-//   return {
-//     addToCart,
-//     cart,
-//     totalPrice,
-//     totalQuantity,
-//   };
-// })();
-// // IIFE(Immediately Invoked Function Expression) 을 통해 최초 1회 실행시키고,
-// // module의 export 를 return으로 대신함.
-// // import 하여 사용할 변수, 함수들을 return시키고 ShoppingCart2라는 변수에 할당시킴.
-// // 이 모든것이 closure(클로저)를 통해 가능하게 됨.
+// //   return {
+// //     addToCart,
+// //     cart,
+// //     totalPrice,
+// //     totalQuantity,
+// //   };
+// // })();
+// // // IIFE(Immediately Invoked Function Expression) 을 통해 최초 1회 실행시키고,
+// // // module의 export 를 return으로 대신함.
+// // // import 하여 사용할 변수, 함수들을 return시키고 ShoppingCart2라는 변수에 할당시킴.
+// // // 이 모든것이 closure(클로저)를 통해 가능하게 됨.
 
-// ShoppingCart2.addToCart('apple', 4);
-// ShoppingCart2.addToCart('pizza', 2);
+// // ShoppingCart2.addToCart('apple', 4);
+// // ShoppingCart2.addToCart('pizza', 2);
 
-// console.log(ShoppingCart2);
-// console.log(ShoppingCart2.shippingCost);
+// // console.log(ShoppingCart2);
+// // console.log(ShoppingCart2.shippingCost);
 
-////////////////////////////////////////////////////////
-// // CommonJS Modules ( Browser에서 동작 안하더라도 Node js 에서 동작함.)
-// Export
-// export.addToCart = function (product, quantity) {
-//   cart.push({ product, quantity });
-//   console.log(
-//     `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
-//   );
-// };
+// ////////////////////////////////////////////////////////
+// // // CommonJS Modules ( Browser에서 동작 안하더라도 Node js 에서 동작함.)
+// // Export
+// // export.addToCart = function (product, quantity) {
+// //   cart.push({ product, quantity });
+// //   console.log(
+// //     `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+// //   );
+// // };
 
-// // Import
-// const {addToCart} = require('./shoppingCart.js')
+// // // Import
+// // const {addToCart} = require('./shoppingCart.js')
 
-// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
-import cloneDeep from 'lodash-es';
+// // import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
 // import cloneDeep from 'lodash-es';
+// // import cloneDeep from 'lodash-es';
 
 const state = {
   cart: [
@@ -120,17 +120,38 @@ const state = {
   user: { loggedIn: true },
 };
 
-const stateClone = Object.assign({}, state);
+// const stateClone = Object.assign({}, state);
+// // state.user.loggedIn = false;
+// console.log(stateClone);
+
+// const stateDeepClone = cloneDeep(state); // Good solution to Deep Clone
 // state.user.loggedIn = false;
-console.log(stateClone);
+// console.log(stateDeepClone);
 
-const stateDeepClone = cloneDeep(state); // Good solution to Deep Clone
-state.user.loggedIn = false;
-console.log(stateDeepClone);
+// if (module.hot) {
+//   // module.hot은 parcel에서만 사용.
+//   // hot module reloading이 뜻하는 것은, 우리가 모듈 중 하나를 변경했을 때, rebuild를 trigger 함.
+//   // 그리고 페이지를 새로고침하지 않고 바로 페이지에 반영됨.
+//   module.hot.accept();
+// }
 
-if (module.hot) {
-  // module.hot은 parcel에서만 사용.
-  // hot module reloading이 뜻하는 것은, 우리가 모듈 중 하나를 변경했을 때, rebuild를 trigger 함.
-  // 그리고 페이지를 새로고침하지 않고 바로 페이지에 반영됨.
-  module.hot.accept();
+class Person {
+  greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.greeting}, ${this.name}`);
+  }
 }
+const dohyun = new Person('Dohyun');
+
+console.log(dohyun ?? null);
+
+console.log(state.cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+import 'core-js/stable';
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/array/promise';
+
+// Polifilling async function
+import 'regenerator-runtime/runtime';
