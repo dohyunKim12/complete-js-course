e98346dfa2643637957ab3da4d490cb9763cc438
dohyunkim <kdh951102@gmail.com>
2022-05-25 16:22:20 +0900

    Forkify - Uploading new Recipe

--- 18-forkify/starter/src/js/config.js
+++ 18-forkify/starter/src/js/config.js
@@ -2,3 +2,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '161442aa-b185-482f-a2ba-2121caee1e0f';
+export const MODAL_CLOSE_SEC = 2;
